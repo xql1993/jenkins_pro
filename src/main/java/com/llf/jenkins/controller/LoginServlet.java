66745341093eb13180cc95f0cddbf130477be5a7
xql1993 <799631291@qq.com>
2018-09-01 17:32:14 +0800

    update in eclipse...

--- src/main/java/com/llf/jenkins/controller/LoginServlet.java
+++ src/main/java/com/llf/jenkins/controller/LoginServlet.java
@@ -21,7 +21,7 @@ public class LoginServlet extends HttpServlet {
 		String userName = request.getParameter("userName");
 		String pswd = request.getParameter("pswd");
 		System.out.println(userName + " " + pswd);
-		if(userName.equals("llf1993") && pswd.equals("xql1993")){
+		if(userName.equals("19931027") && pswd.equals("19930307")){
 			request.setAttribute("userName", userName);
 			request.getRequestDispatcher("jsp/main.jsp").forward(request, response);
 		}else{
